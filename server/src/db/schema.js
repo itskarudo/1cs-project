da01b4080279d6e71c7e857bf9eaaec79c9d4fc4
0xfrmr <houtarou.fr@gmail.com>
2024-03-21 13:40:53 +0100

    add user schema

--- server/src/db/schema.js
+++ server/src/db/schema.js
@@ -1,6 +1,13 @@
 import { mysqlTable, serial, varchar } from "drizzle-orm/mysql-core";
 
-export const exampleTable = mysqlTable("example", {
+export const User = mysqlTable("User", {
   id: serial("id").primaryKey(),
-  string: varchar("ss", { length: 256 }),
+  firstName: varchar("first_name", { length: 256 }).required(),
+  lastName: varchar("last_name", { length: 256 }).required(),
+  email: varchar("email", { length: 256 }).required(),
+  password: varchar("password", { length: 256 }).required(),
+  grade: mysqlEnum("grade", ["Professeur", "enseignant","Assistant Master A"
+   ,"Assistant Master B", "Lecturer A","Lecturer B"]).required(),
+  role: mysqlEnum("role",["admin","enseignant"]),
 });
+
