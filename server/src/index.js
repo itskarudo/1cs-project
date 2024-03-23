fb19b5f5f288a907317a630b331ef0e14a7b4202
Amdjed Zerrougui <102095228+amj3x@users.noreply.github.com>
2024-03-23 21:32:09 +0100

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import exampleRouter from './routes/example.js';
 import authRouter from './routes/authRoutes.js';
 
