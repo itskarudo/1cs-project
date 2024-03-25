efe9721bdbaadadceced295d8df52c2fac1dc520
0xfrmr <houtarou.fr@gmail.com>
2024-03-25 12:53:23 +0100

    added cors

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,14 @@ import express from 'express';
 import dotenv from 'dotenv';
 import exampleRouter from './routes/example.js';
 import authRouter from './routes/authRoutes.js';
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors({origin: true, credentials: true}))
 app.use('/', exampleRouter);
-
 app.use('/auth', authRouter);
 
 app.listen(3000, () => {
