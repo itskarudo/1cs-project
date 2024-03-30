6e370eb23f16abed959596d8c2e873a877e9ce4a
ayabkr <s.boukharouba@esi-sba.dz>
2024-03-30 17:17:01 +0100

    changes

---
+++ client/src/pages/Calendrier.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export const Calendrier = () => {
+    return <div>Calendrier</div>;
+}
---
+++ client/src/pages/Notifications.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export const Notifications = () => {
+    return <div>Notifications</div>;
+}
